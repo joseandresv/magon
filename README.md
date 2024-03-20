80eafa6775ffe9adcd19b7f1d90bd8caf6585bcb
uumami <vazcorm@gmail.com>
2024-03-20 15:56:14 -0600

    mongodb  code included

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Repositorio de ChatBot Magon
 `pip install -r requirements_demo.txt`
 
 ## ChatGPT token
-PAra que funcione deben poner el token de su API de ChatGPT en el archivo `.env`, un ejemplo de como debe verse ese archivo esta en `.env_example`. Crea uno que se llame `.env` en tu local, este archivo esta protegido por el `.gitignore` para no subirse a github, sin embargo se cuidadose.
+PAra que funcione deben poner el token de su API de ChatGPT en el archivo `.env`, un ejemplo de como debe verse ese archivo esta en `.env_example`. Crea uno que se llame `.env` en tu local, este archivo esta protegido por el `.gitignore` para no subirse a github, sin embargo se cuidadose. Si necesitas uno escribeme y te comparto el mio.
 
 ## Demo
 Primero se debe correr el `scrapping` para bajar las noticias del tema, despues el `ChatBot` para responder preguntas. El demo solo utiliza los articulos que descargaste.
@@ -16,6 +16,8 @@ Primero se debe correr el `scrapping` para bajar las noticias del tema, despues 
 ### ChatBot
 `02_paperqa_demo.ipynb` demo para hacer preguntas
 
+### MongoDB database
+Por ahora para guardar los documentos existe una base de datos de MongoDB en la nube. Para entender como funciona utiliza el `03_def_mongodb.ipynb`. Agrega el `client` a tu `.env`. Ve el ejemplo en `.env_example`
 
 ## TODO
 - [ ] Crear pip.lock
--- scrapper/mongodb.py
+++ scrapper/mongodb.py
@@ -1,26 +1,58 @@
 from pymongo import MongoClient
 
 class DatabaseManager:
-    def __init__(self, database_name, collection_name):
-        self.client = MongoClient('mongodb://localhost:27017/')
+    def __init__(self, client, database_name, collection_name):
+        self.client = MongoClient(client)
         self.database_name = database_name
         self.collection_name = collection_name
-        self.db = self.client[self.database_name]
-        self.collection = self.db[self.collection_name]
+        self.db = None
+        self.collection = None
+
+    def create_database(self):
+        if self.database_name not in self.client.list_database_names():
+            self.db = self.client[self.database_name]
+            print(f"Created database: {self.database_name}")
+        else:
+            self.db = self.client[self.database_name]
+            print(f"Database {self.database_name} already exists")
+
+    def create_collection(self):
+        if self.collection_name not in self.db.list_collection_names():
+            self.collection = self.db[self.collection_name]
+            print(f"Created collection: {self.collection_name}")
+        else:
+            self.collection = self.db[self.collection_name]
+            print(f"Collection {self.collection_name} already exists")
 
     def insert_document(self, document):
+        if self.collection is None:
+            print("Collection is not defined. Please create the collection first.")
+            return
+
         result = self.collection.insert_one(document)
         print(f"Inserted document with ID: {result.inserted_id}")
 
     def find_document(self, query):
+        if self.collection is None:
+            print("Collection is not defined. Please create the collection first.")
+            return None
+
         document = self.collection.find_one(query)
         return document
 
     def update_document(self, query, update):
+        if self.collection is None:
+            print("Collection is not defined. Please create the collection first.")
+            return
+
         result = self.collection.update_one(query, update)
         print(f"Modified {result.modified_count} document(s)")
 
     def delete_document(self, query):
+        if self.collection is None:
+            print("Collection is not defined. Please create the collection first.")
+            return
+
         result = self.collection.delete_one(query)
         print(f"Deleted {result.deleted_count} document(s)")
 
